dd26ea97e079492595ba0bdcb62c889a92852a21
Gihoon Lim <knv429@naver.com>
2023-12-12 19:18:01 +0900

    styled components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, { keyframes } from "styled-components";
+
+const animation = keyframes`
+  from {
+    transform: rotate(0deg);
+    border-radius: 0px;
+    background-color: teal;
+  }
+  to {
+    transform: rotate(360deg);
+    border-radius: 100px;
+    background-color: tomato;
+  }
+`;
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${animation} 1s linear infinite;
+  span {
+    font-size: 36px;
+    &:hover {
+      font-size: 40px;
+    }
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Father>
+      <Box bgColor="teal">
+        <span>🌝</span>
+      </Box>
+    </Father>
   );
 }
 
