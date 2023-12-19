1c22b075d97937882576af9c5215eefafb66527a
Gihoon Lim <knv429@naver.com>
2023-12-19 22:46:54 +0900

    BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
-import styled from "styled-components";
+import Router from "./Router";
 
 function App() {
-  const Container = styled.div`
-    background-color: ${(props) => props.theme.bgColor};
-  `;
-  const H1 = styled.h1`
-    color: ${(props) => props.theme.textColor};
-  `;
   return (
-    <Container>
-      <H1>Protect</H1>
-    </Container>
+    <div>
+      <Router />
+    </div>
   );
 }
 
--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,18 @@
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./screens/components/Header";
+import Home from "./screens/Home";
+import About from "./screens/About";
 
 function Router() {
-  return <BrowserRouter>
-    <Header/>
-    <Routes></Routes>
-  </BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export Router;
+export default Router;
