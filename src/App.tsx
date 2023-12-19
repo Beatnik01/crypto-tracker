d89ccd121d78633e4d22c1871e5dad9ed8e153fa
Gihoon Lim <knv429@naver.com>
2023-12-19 22:21:18 +0900

    install react-router-dom@5.3.4

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import Circle from "./Circle";
+import styled from "styled-components";
 
 function App() {
+  const Container = styled.div`
+    background-color: ${(props) => props.theme.bgColor};
+  `;
+  const H1 = styled.h1`
+    color: ${(props) => props.theme.textColor};
+  `;
   return (
-    <div>
-      <Circle bgColor="teal" borderColor="tomato" />
-      <Circle bgColor="tomato" />
-    </div>
+    <Container>
+      <H1>Protect</H1>
+    </Container>
   );
 }
 
