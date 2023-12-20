b9743d8cd191b5cc4fe074bb9ac0c104e80a0a0b
Gihoon Lim <knv429@naver.com>
2023-12-21 08:02:19 +0900

    useNavigate & useParams

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router";
-import Header from "./screens/components/Header";
+import Header from "./components/Header";
 
 function Root() {
   return (
---
+++ src/db.ts
@@ -0,0 +1,10 @@
+export const users = [
+  {
+    id: 1,
+    name: "fo",
+  },
+  {
+    id: 2,
+    name: "tofu",
+  },
+];
