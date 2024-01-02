178ae189e0cac6cf15b4573860b1746aa95959ae
Gihoon Lim <knv429@naver.com>
2024-01-02 16:20:02 +0900

    update react-dom -> react-dom/client

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 import router from "./Router";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+if (!container) throw new Error("Failed to find the root element!");
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <RouterProvider router={router} />
     </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
